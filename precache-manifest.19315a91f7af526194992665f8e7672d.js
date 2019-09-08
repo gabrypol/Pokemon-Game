6129873b91877e317cca4a66b5fe19b5f06ad222
gabrypol <gabrielepolenta@gmail.com>
2019-09-08 11:04:45 +0200

    Updates

---
+++ precache-manifest.19315a91f7af526194992665f8e7672d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd2f3e60a5730c7b492f1de58316119",
+    "url": "/Pokemon-Game/index.html"
+  },
+  {
+    "revision": "7e407772663ce57bcf18",
+    "url": "/Pokemon-Game/static/css/main.73c19bf4.chunk.css"
+  },
+  {
+    "revision": "c3473b42bc6ea06b8fcb",
+    "url": "/Pokemon-Game/static/js/2.99cfcc6f.chunk.js"
+  },
+  {
+    "revision": "7e407772663ce57bcf18",
+    "url": "/Pokemon-Game/static/js/main.f581882c.chunk.js"
+  },
+  {
+    "revision": "500c2fbeb777c5528112",
+    "url": "/Pokemon-Game/static/js/runtime~main.aaa3fd37.js"
+  }
+]);
